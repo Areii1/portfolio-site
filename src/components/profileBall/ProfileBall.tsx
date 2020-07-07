3f840ce095ebcf0f57d6a705d47c605bd9b7cef8
ari-pekka jääskeläinen <ari.jaaskelainen1@gmail.com>
2020-07-08 01:16:46 +0300

    add profile ball

---
+++ src/components/profileBall/ProfileBall.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import profile from "../../assets/profile.jpg";
+
+type Props = {
+  picture: string;
+};
+
+const Wrapper = styled.div`
+  width: 350px;
+  height: 350px;
+  border-radius: 350px;
+  background-color: #e7e7e7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProfileImage = styled.img`
+  display: inline;
+  margin: 0 auto;
+  margin-left: -25%;
+  height: 100%;
+  width: auto;
+`;
+
+const ProfileImageWrapper = styled.div`
+  width: 315px;
+  height: 315px;
+  position: relative;
+  overflow: hidden;
+  border-radius: 50%;
+`;
+
+export const ProfileBall = (props: Props) => {
+  return (
+    <Wrapper>
+      <ProfileImageWrapper>
+        <ProfileImage src={profile} alt="profile" />
+      </ProfileImageWrapper>
+    </Wrapper>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import SEO from "../components/seo";
+import styled from "styled-components";
 import { Header } from "../components/header/Header";
 import { ContentEn } from "../ContentEn";
 import { ContentFi } from "../ContentFi";
 import "./index.css";
+import { ProfileBall } from "../components/profileBall/ProfileBall";
 
 export enum lan {
   FINNISH,
   ENGLISH,
 }
+
+const ProfileBallWrapper = styled.div`
+  position: absolute;
+  top: 425px;
+  left: calc(50% - 175px);
+`;
 
 const IndexPage = () => {
   const [language, setLanguage] = React.useState<lan>(lan.FINNISH);
@@ -24,6 +32,9 @@ const IndexPage = () => {
         jobTitle={content.profile.jobTitle}
         updateLanguage={updateLanguage}
       />
+      <ProfileBallWrapper>
+        <ProfileBall picture="sd" />
+      </ProfileBallWrapper>
     </div>
   );
 };
